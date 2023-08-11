6054b8ca98a0b20507d89f9a0aaa896803c51348
Pechinkin Sergey <pechinkinsergey@yandex.ru>
2023-08-11 23:09:19 +0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const res = n.toString().split('').reverse().join('');
+  if (res.includes('-')){
+    return +res.slice(0, -1);
+  } else {
+    return +res;
+  }
 }
